836d7bdcbba453c19284571e89b5c83645caea57
jjvega86 <jj.vega86@gmail.com>
2020-09-15 13:31:58 -0400

    Added a DbSet property to represent a table of Superheroes to ApplicationDbContext.cs

--- Superhero_Creator/Data/ApplicationDbContext.cs
+++ Superhero_Creator/Data/ApplicationDbContext.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Superhero_Creator.Models;
 
 namespace Superhero_Creator.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Superhero> Superheroes { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
