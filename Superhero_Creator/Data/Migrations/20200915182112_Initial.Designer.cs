2fc23fe53e0f192cd2e0f9922f5e19a1280f2f61
jjvega86 <jj.vega86@gmail.com>
2020-09-15 14:24:22 -0400

    Fixed migration command error with file convention rename

--- Superhero_Creator/Data/Migrations/20200915182112_Initial.Designer.cs
+++ Superhero_Creator/Data/Migrations/20200915182112_Initial.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Superhero_Creator.Data;
 
 namespace Superhero_Creator.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200915182112_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
